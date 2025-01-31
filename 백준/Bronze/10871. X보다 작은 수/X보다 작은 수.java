5839f2063d014790d6f8102e6f4abe5feb6b4769
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-01-31 17:52:10 +0900

    [Bronze V] Title: X보다 작은 수, Time: 452 ms, Memory: 30764 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int X = sc.nextInt();
+        int array[] = new int[N];
+
+        for(int i=0;i<array.length;i++) {
+			array[i] = sc.nextInt();
+		}
+
+        for(int i=0; i<array.length; i++) {
+            if(array[i] < X) {
+                System.out.print(array[i] + " ");
+            }
+        }
+        sc.close();
+    }
+}
