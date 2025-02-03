de7175bc70fe103ad87429576c2929fb178b39fb
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-02-03 15:55:11 +0900

    [level 0] Title: 특수문자 출력하기, Time: 183.46 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -3,5 +3,6 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         System.out.println("!@#$%^&*(\\\'\"<>?:;");
+                           
     }
 }
