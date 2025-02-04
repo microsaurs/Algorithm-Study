16a427ac94d43b3d18f7f5ca2e27bb3bd44852a4
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-02-04 10:28:09 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 2.01 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,9 +1,11 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        int len1 = my_string.length();
-        int len2 = overwrite_string.length();
+        String answer = my_string.substring(0, s) + overwrite_string;
+
+        if(my_string.length() > answer.length()) {
+            answer += my_string.substring(answer.length());
+        }
         
-        return my_string.substring(0,s) + overwrite_string 
-            + my_string.substring(s+len2, len1);
+        return answer;
     }
 }
