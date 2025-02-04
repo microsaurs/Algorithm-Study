53201a172d56044b1af1eea5a00806b796bfd9c3
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-02-04 13:13:29 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
@@ -1,14 +1,21 @@
 class Solution {
     public int solution(int n) {
-        int sum = 0;
+        int answer = 0;
         
-        for(int i=1; i<=n; i++) {
-            if(n%2==1 && i%2==1) {
-                sum += i;
-            } else if(n%2==0 && i%2==0) {
-                sum += i*i;
+        if(n%2 == 1) {
+            for(int i=1; i<=n; i++) {
+                if(i%2==1) {
+                    answer += i;
+                }
+            }
+        } else {
+            for(int i=1; i<=n; i++) {
+                if(i%2==0) {
+                    answer += i*i;
+                }
+                
             }
         }
-         return sum;
+        return answer;
     }
 }
