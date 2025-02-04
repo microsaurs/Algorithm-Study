0fb890505fb3672a0796928f995af66db67752d8
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-02-04 11:04:11 +0900

    [level 0] Title: 문자열 섞기, Time: 0.06 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,11 +1,14 @@
 class Solution {
     public String solution(String str1, String str2) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
+        char[] char1 = str1.toCharArray();
+        char[] char2 = str2.toCharArray();
         
-        for(int i=0; i<str1.length(); i++) {
-             answer += str1.substring(i, i+1);
-             answer += str2.substring(i, i+1);
+        for(int i=0; i<str1.length(); i++){
+            answer.append(str1.charAt(i)); 
+            answer.append(str2.charAt(i)); 
         }
-        return answer;
+
+        return answer.toString(); 
     }
 }
