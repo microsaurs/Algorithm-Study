5a282b1f4f2930b014837bfe831b48554c16daa7
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-02-05 16:09:53 +0900

    [level 0] Title: 조건 문자열, Time: 0.03 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -1,17 +1,17 @@
 class Solution {
     public int solution(String ineq, String eq, int n, int m) {
-        int answer = 0;
-        
-        if(eq.equals("=") && ineq.equals(">")){
-            answer = n >= m ? 1 : 0 ;
-        } else if (eq.equals("=") && ineq.equals("<")){
-             answer = n <= m ? 1 : 0 ;
-        } else if (eq.equals("!") && ineq.equals(">")){
-             answer = n > m ? 1 : 0 ;
-        } else if (eq.equals("!") && ineq.equals("<")){
-             answer = n < m ? 1 : 0 ;
-        }
-        
-        return answer;
+       if(ineq.equals("<")) {
+           if(eq.equals("=")) {
+               return n <= m ? 1 : 0;
+           } else {
+               return n < m ? 1 : 0;
+           }
+       } else {
+            if(eq.equals("=")) {
+               return n >= m ? 1 : 0;
+           } else {
+               return n > m ? 1 : 0;
+           }
+       }
     }
 }
