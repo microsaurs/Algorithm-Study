f9fdcdd6b8af1efd2e4936f0e0b0f7ebdb2b05cb
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-02-20 15:21:22 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 94.5 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/README.md
+++ 프로그래머스/0/181930. 주사위 게임 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 주사위 게임 2 - 181930 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181930?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181930) 
 
 ### 성능 요약
 
-메모리: 84.9 MB, 시간: 0.03 ms
+메모리: 94.5 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 16일 23:43:42
+2025년 02월 20일 15:21:20
 
 ### 문제 설명
 
--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,6 +1,11 @@
 class Solution {
     public int solution(int a, int b, int c) {
-        int answer = 0;
-        return answer;
+        if(a == b && a == c) {
+            return (a+b+c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
+        } else if( a!= b && a !=c && b!=c) {
+            return a+b+c;
+        } else {
+            return (a+b+c) * (a*a + b*b + c*c);
+        }
     }
 }
