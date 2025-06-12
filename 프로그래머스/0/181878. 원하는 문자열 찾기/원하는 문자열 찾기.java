a22757f0482ee8917600db5f418cae533b9f34d0
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-06-12 16:33:16 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 7.33 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        
+        return ((myString.toLowerCase()).contains(pat.toLowerCase())) ? 1 : 0;
+        
+    }
+}
