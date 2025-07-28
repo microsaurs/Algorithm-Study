d48933d5c674413fb02ebb938d5598605dc63b01
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-07-28 19:06:31 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        HashMap<String, Integer> clothesMap = new HashMap<>();
+        
+        for (String[] cloth : clothes) {
+            String type = cloth[1];
+            clothesMap.put(type, clothesMap.getOrDefault(type, 0) + 1);
+        }
+        
+        int answer = 1;
+        for (int count : clothesMap.values()) {
+            answer *= (count + 1);
+        }
+        
+        return answer - 1;
+    }
+}
