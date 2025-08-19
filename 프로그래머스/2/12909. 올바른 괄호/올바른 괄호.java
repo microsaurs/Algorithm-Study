2b69d45890fcb99547478e48135cd0340e9bab88
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-08-19 12:13:41 +0900

    [level 2] Title: 올바른 괄호, Time: 5.99 ms, Memory: 54.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,20 @@
+class Solution {
+    boolean solution(String s) {
+        int count = 0;
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                count++;
+            } else if (c == ')') {
+                count--;
+                
+                if (count < 0) {
+                    return false;
+                }
+            }
+        }
+        
+        return count == 0;
+
+    }
+}
