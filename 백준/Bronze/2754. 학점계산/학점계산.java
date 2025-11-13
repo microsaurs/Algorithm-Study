6123230deef3862d768badba92791b2016b0a925
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-11-13 11:18:19 +0900

    [Bronze III] Title: 학점계산, Time: 100 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -0,0 +1,33 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String s = br.readLine();
+        double grade = 0;
+        char c = s.charAt(0);
+        
+        if(c == 'A') {
+            grade = 4;
+        } else if (c == 'B') {
+            grade = 3;
+        } else if (c == 'C') {
+            grade = 2;
+        } else if (c == 'D') {
+            grade = 1;
+        } else {
+            System.out.print("0.0");
+            return;
+        }
+        
+        c = s.charAt(1);
+        if(c == '+') {
+            grade += 0.3;
+        } else if (c == '-') {
+            grade -= 0.3;
+        }
+        
+        System.out.print(grade);
+        
+    }
+}
