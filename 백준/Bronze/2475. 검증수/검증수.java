74ba29fb932eafdcd8ce9a348678da0efa1ce129
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-11-14 11:47:19 +0900

    [Bronze V] Title: 검증수, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());        
+        int sum = 0;
+        
+        while(st.hasMoreTokens()) {
+            int a = Integer.parseInt(st.nextToken());
+            sum += a*a;
+        }
+        System.out.print(sum % 10);
+    }
+}
