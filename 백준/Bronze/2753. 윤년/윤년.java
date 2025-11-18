bdd03a074e2b5e6cc94941b4c25180415cfc5a5b
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-11-18 20:48:48 +0900

    [Bronze V] Title: 윤년, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,14 +1,14 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main (String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int A = sc.nextInt();
-        int result = (((A % 4 == 0) && (A % 100 != 0)) || (A % 400 == 0)) ? 1 : 0;
-
-        System.out.println(result);
- 
-        sc.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(br.readLine());
+        
+        if(year % 4 == 0 && year % 100 != 0 || year % 400 ==0) {
+            System.out.print("1");
+        } else {
+            System.out.print("0");
+        }
     }
 }
