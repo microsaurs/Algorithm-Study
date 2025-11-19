a5d2138b5de1aead2ae691d499c6984be6633549
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-11-19 09:57:59 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\\n");
+		sb.append(" )  ( ')\n");
+		sb.append("(  /  )\n");
+ 		sb.append(" \\(__)|\n");
+        
+        System.out.println(sb);
+    }
+}
