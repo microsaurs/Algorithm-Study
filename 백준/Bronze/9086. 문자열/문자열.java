b044f76461f478d11d11cdf47467a010a50c324c
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-11-22 15:29:46 +0900

    [Bronze V] Title: 문자열, Time: 92 ms, Memory: 13960 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,16 +1,19 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
         
-        for(int i=0; i<n; i++) {
+        for(int i=0; i<T; i++) {
             String s = br.readLine();
-            
-            System.out.print(s.charAt(0));
-            System.out.println(s.charAt(s.length()-1));
+            sb.append(s.charAt(0)).append(s.charAt(s.length() -1)).append("\n");
         }
-        br.close();
+
+        System.out.println(sb);
     }
-} 
+}
