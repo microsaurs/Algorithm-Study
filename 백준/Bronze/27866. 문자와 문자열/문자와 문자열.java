17f59c579c430b25ae7464f3373e45828a29ee4b
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-11-22 15:23:35 +0900

    [Bronze V] Title: 문자와 문자열, Time: 96 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -1,11 +1,14 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        int n = Integer.parseInt(br.readLine());
         
-        System.out.println(s.charAt(n-1));
+        String S = br.readLine();
+        int i = Integer.parseInt(br.readLine());
+
+        System.out.print(S.charAt(i-1));
     }
 }
