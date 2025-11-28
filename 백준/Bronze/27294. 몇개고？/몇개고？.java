84f73a2e8e150bcee969d622516ff04d4803b6ec
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-11-28 14:48:16 +0900

    [Bronze V] Title: 몇개고?, Time: 108 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/27294. 몇개고？/몇개고？.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int T = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+
+        if(T >= 12 && T <= 16) {
+            if(S == 0) {
+                System.out.println(320);
+            } else {
+                System.out.println(280);
+            }
+        } else {
+            System.out.println(280);
+        }
+        
+    }
+}
+
