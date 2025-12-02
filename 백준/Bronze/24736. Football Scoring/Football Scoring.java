4dcf29ac44886962812a62002f98df55ad3681bc
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-12-02 09:59:44 +0900

    [Bronze V] Title: Football Scoring, Time: 100 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        for(int i=0; i<2; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            
+            System.out.println(a*6 + b*3 + c*2 + d*1 + e*2);
+        }
+    }
+}
+
