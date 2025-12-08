8556d4ea5ee9cf5d0acfaea1ccac4a1a67225344
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-12-08 17:28:10 +0900

    [Bronze V] Title: Serca, Time: 828 ms, Memory: 39888 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append(" @@@   @@@ ").append('\n');
+        sb.append("@   @ @   @").append('\n');
+        sb.append("@    @    @").append('\n');
+        sb.append("@         @").append('\n');
+        sb.append(" @       @ ").append('\n');
+        sb.append("  @     @  ").append('\n');
+        sb.append("   @   @   ").append('\n');
+        sb.append("    @ @   ").append('\n');
+        sb.append("     @     ").append('\n');
+
+        for(int i=0; i<n; i++) {
+            System.out.print(sb);
+        }
+    }
+}
+
