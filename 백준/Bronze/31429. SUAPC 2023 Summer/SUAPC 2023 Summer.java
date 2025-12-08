132377bdaae4646b7fa7b7de9191e64c016fe437
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-12-08 18:56:33 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 120 ms, Memory: 16128 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        int[][] ranking = {
+            {},
+            {12, 1600},
+            {11, 894},
+            {11, 1327},
+            {10, 1311},
+            {9, 1004},
+            {9, 1178},
+            {9, 1357},
+            {8, 837},
+            {7, 1055},
+            {6, 556},
+            {6, 773}
+        };
+
+        System.out.println(ranking[n][0] + " " + ranking[n][1]);    
+    }
+}
+
