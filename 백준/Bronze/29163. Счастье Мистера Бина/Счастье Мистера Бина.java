4cbbeb8a344d000dc014e47da20132bc1e45f5a1
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-12-09 18:00:03 +0900

    [Bronze V] Title: Счастье Мистера Бина, Time: 112 ms, Memory: 14420 KB -BaekjoonHub

---
+++ 백준/Bronze/29163. Счастье Мистера Бина/Счастье Мистера Бина.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int even = 0;
+        int odd = 0;
+
+        for(int i=0; i<n; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            if(x % 2 == 0) {
+                odd += 1;
+            } else {
+                even +=1;
+            }
+        }
+
+        System.out.println(even < odd ? "Happy" : "Sad");
+    }
+}
