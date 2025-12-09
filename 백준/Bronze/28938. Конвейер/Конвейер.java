3b18c3038c1134e96c4aef7e7318754c5ad8574e
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-12-09 18:19:13 +0900

    [Bronze V] Title: Конвейер, Time: 108 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Bronze/28938. Конвейер/Конвейер.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int sum = 0;
+
+        for(int i=0; i<n; i++) {
+            sum += Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(sum > 0 ? "Right" : sum < 0 ?  "Left" : "Stay");
+
+    }
+}
