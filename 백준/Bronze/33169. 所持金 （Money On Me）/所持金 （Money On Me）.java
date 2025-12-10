84ab30650ee748eff53437007fc9155720dfa273
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-12-10 12:38:08 +0900

    [Bronze V] Title: 所持金 (Money On Me), Time: 100 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Bronze/33169. 所持金 （Money On Me）/所持金 （Money On Me）.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        System.out.print(1000*a + 10000*b);
+    }
+}
