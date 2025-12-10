0be408500572fdc5330b6c37b09a6c57aacb2d3c
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-12-10 13:29:42 +0900

    [Bronze V] Title: 공통교육과정, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/34813. 공통교육과정/공통교육과정.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        if(s.charAt(0) == 'F') {
+            System.out.println("Foundation");
+        } else if (s.charAt(0) == 'C') {
+            System.out.println("Claves");
+        } else if (s.charAt(0) == 'V') {
+            System.out.println("Veritas");
+        } else {
+            System.out.println("Exploration");
+        }
+    }
+}
