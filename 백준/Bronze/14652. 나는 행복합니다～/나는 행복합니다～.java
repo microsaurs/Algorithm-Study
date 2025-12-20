2d1f440da18b5c88dfab7e56f4909d2da61553c7
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2025-12-20 16:59:31 +0900

    [Bronze IV] Title: 나는 행복합니다~, Time: 124 ms, Memory: 15996 KB -BaekjoonHub

---
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int d = c / b;
+        int e = c % b;
+
+        System.out.println(d + " " + e);
+        
+    }
+}
