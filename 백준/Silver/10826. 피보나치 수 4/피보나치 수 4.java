209e3815df2299d0cdae6bd3050a3b72e75fd92f
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2026-01-27 22:13:45 +0900

    [Silver V] Title: 피보나치 수 4, Time: 144 ms, Memory: 21364 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+    
+        BigInteger[] fib = new BigInteger[n+2];
+
+        fib[0] = BigInteger.ZERO;
+        fib[1] = BigInteger.ONE;
+        for(int i=2; i<=n; i++) {
+            fib[i] = fib[i-1].add(fib[i-2]);
+        } 
+
+        System.out.println(fib[n]);
+    }
+}
