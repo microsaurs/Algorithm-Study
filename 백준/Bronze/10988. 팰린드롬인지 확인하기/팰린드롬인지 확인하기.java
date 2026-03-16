7ceac0a074ffbc37a38dea3a1b9120af78f9c585
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2026-03-16 19:15:39 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 104 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        StringBuffer sb = new StringBuffer(s);
+        String reverse = sb.reverse().toString();
+
+        System.out.println(s.equals(reverse) ? 1 : 0);
+        
+    }
+}
