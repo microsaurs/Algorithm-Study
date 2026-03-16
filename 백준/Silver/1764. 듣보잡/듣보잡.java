85a3b0d498e94dee2693e47cace4659f6fc949bf
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2026-03-16 18:46:38 +0900

    [Silver IV] Title: 듣보잡, Time: 280 ms, Memory: 26296 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        HashSet<String> hear = new HashSet<>();
+
+        for(int i=0; i<n; i++) {
+            hear.add(br.readLine());
+        }
+
+        ArrayList<String> result = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            String name = br.readLine();
+            if (hear.contains(name)) {
+                result.add(name);
+            }
+        }
+
+        Collections.sort(result);
+
+        System.out.println(result.size());
+
+        for (String name : result) {
+            System.out.println(name);
+        }
+    }
+}
