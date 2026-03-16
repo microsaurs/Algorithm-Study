7519767ceed95002815a6a458b7dcdb4903e3fac
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2026-03-16 19:29:59 +0900

    [Bronze III] Title: 최소, 최대, Time: 440 ms, Memory: 88256 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for(int i=0;i<n;i++){
+            int num = Integer.parseInt(st.nextToken());
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+        }
+
+        System.out.println(min + " " + max);
+    }
+}
