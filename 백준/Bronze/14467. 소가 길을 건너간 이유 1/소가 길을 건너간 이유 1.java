8fd17af985818ab33f90afea17dffb722a4948f2
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2026-03-17 15:14:11 +0900

    [Bronze I] Title: 소가 길을 건너간 이유 1, Time: 100 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        int[] cows = new int[11];
+        Arrays.fill(cows, -1);
+        int count = 0;
+
+        for(int i=0; i<n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            int dir = Integer.parseInt(st.nextToken());
+            
+            if(cows[num] == -1) {
+                cows[num] = dir;
+            } else if (cows[num] != dir) {
+                count++;
+                cows[num] = dir;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
