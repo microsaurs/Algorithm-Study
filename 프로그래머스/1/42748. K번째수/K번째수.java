df4dea8d7b3598e0337c7fd9c319eb4a5f7cc55c
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2026-03-18 16:50:54 +0900

    [level 1] Title: K번째수, Time: 1.34 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,16 +4,18 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
-        for (int idx = 0; idx < commands.length; idx++) {
-            int i = commands[idx][0]; 
-            int j = commands[idx][1];  
-            int k = commands[idx][2]; 
+        for(int idx=0; idx<commands.length; idx++) {
+            int i = commands[idx][0];
+            int j = commands[idx][1];
+            int k = commands[idx][2];
             
-            int[] subarray = Arrays.copyOfRange(array, i - 1, j);
-            
-            Arrays.sort(subarray);
-            
-            answer[idx] = subarray[k - 1];
+            int[] arr = new int[j-i+1];
+            int index = 0;
+            for(int a = i-1; a <= j-1; a++) {
+                arr[index++] = array[a];
+            }
+            Arrays.sort(arr);
+            answer[idx] = arr[k-1];
         }
         
         return answer;
