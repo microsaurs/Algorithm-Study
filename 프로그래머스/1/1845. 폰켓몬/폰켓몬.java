42683496fe5de6da84bebc1a9ecdd6ae51b33938
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2026-03-18 14:29:44 +0900

    [level 1] Title: 폰켓몬, Time: 3.82 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,16 +1,17 @@
-import java.util.HashSet;
+import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        HashSet<Integer> set = new HashSet<Integer>();
+        Set<Integer> set = new HashSet<>();
         
-        int i;
-        for (i=0; i < nums.length; i++) {
-             int k = nums[i];
-            set.add(k);
+        for(int i=0; i<nums.length; i++) {
+            set.add(nums[i]);
         }
         
-        return (set.size() >= nums.length / 2) ? (nums.length / 2) : set.size();
-        
+        if(set.size() > nums.length / 2) {
+            return nums.length / 2;
+        } else {
+            return set.size();
+        }
     }
 }
