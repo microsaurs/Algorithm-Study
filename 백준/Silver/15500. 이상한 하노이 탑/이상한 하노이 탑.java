8b7d27687c27c6c57d1e2a27468887fa68224333
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2026-03-24 16:45:31 +0900

    [Silver II] Title: 이상한 하노이 탑, Time: 116 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Silver/15500. 이상한 하노이 탑/이상한 하노이 탑.java
+++ 백준/Silver/15500. 이상한 하노이 탑/이상한 하노이 탑.java
@@ -2,39 +2,55 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static int move = 0;
-
-    static void moveDisk(Deque<Integer> from, Deque<Integer> to, int fromIdx, int toIdx) {
-        to.addLast(from.removeLast());
-        sb.append(fromIdx).append(" ").append(toIdx).append('\n');
-        move++;
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int target = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
         Deque<Integer> s1 = new ArrayDeque<>();
         Deque<Integer> s2 = new ArrayDeque<>();
         Deque<Integer> s3 = new ArrayDeque<>();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        while(st.hasMoreTokens()) {
+        while (st.hasMoreTokens()) {
             s1.addLast(Integer.parseInt(st.nextToken()));
         }
 
-        while(target >= 1) {
-            if(s1.contains(target)) {
-                while(s1.peekLast() != target) {
-                    moveDisk(s1, s2, 1, 2);
+        int target = n;
+        int move = 0;
+        StringBuilder sb = new StringBuilder();
+
+        while (target >= 1) {
+
+            if (s1.contains(target)) {
+
+                while (s1.peekLast() != target) {
+                    int x = s1.removeLast();
+                    s2.addLast(x);
+                    sb.append(1).append(" ").append(2).append('\n');
+                    move++;
                 }
-                moveDisk(s1, s3, 1, 3);
-            } else {
-                while(s2.peekLast() != target) {
-                    moveDisk(s2, s1, 2, 1);
-                }
-                moveDisk(s2, s3, 2, 3);
+
+                int x = s1.removeLast();
+                s3.addLast(x);
+                sb.append(1).append(" ").append(3).append('\n');
+                move++;
             }
+
+            else {
+
+                while (s2.peekLast() != target) {
+                    int x = s2.removeLast();
+                    s1.addLast(x);
+                    sb.append(2).append(" ").append(1).append('\n');
+                    move++;
+                }
+
+                int x = s2.removeLast();
+                s3.addLast(x);
+                sb.append(2).append(" ").append(3).append('\n');
+                move++;
+            }
+
             target--;
         }
 
