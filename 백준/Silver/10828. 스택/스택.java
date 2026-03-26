6c02dc4293ef14e95bd1ce0f5433df4f9fe4ae54
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2026-03-26 16:38:39 +0900

    [Silver IV] Title: 스택, Time: 164 ms, Memory: 19372 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -2,29 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        Stack<Integer> stack = new Stack<>();
+        Deque<Integer> stack = new ArrayDeque<>();
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < N; i++) {
+        for(int i=0; i<n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            String cmd = st.nextToken();
-
-            if (cmd.equals("push")) {
-                stack.push(Integer.parseInt(st.nextToken()));
-            } else if (cmd.equals("top")) {
-                sb.append(stack.isEmpty() ? -1 : stack.peek()).append('\n');
-            } else if (cmd.equals("pop")) {
-                sb.append(stack.isEmpty() ? -1 : stack.pop()).append('\n');
-            } else if (cmd.equals("size")) {
-                sb.append(stack.size()).append('\n');
-            } else if (cmd.equals("empty")) {
-                sb.append(stack.isEmpty() ? 1 : 0).append('\n');
+            String s = st.nextToken();
+            
+            switch (s) {
+                case "push":
+                    stack.push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    sb.append(stack.isEmpty() ? -1 : stack.pop()).append('\n');
+                    break;
+                case "size":
+                    sb.append(stack.size()).append('\n');
+                    break;
+                case "empty":
+                    sb.append(stack.isEmpty() ? 1 : 0).append('\n');
+                    break;
+                case "top":
+                    sb.append(stack.isEmpty() ? -1 : stack.peek()).append('\n');
+                    break;
             }
         }
-        System.out.print(sb.toString());
+        System.out.println(sb);
     }
 }
