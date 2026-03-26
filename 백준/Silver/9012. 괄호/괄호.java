4ce608b22feca66495b61c773baccd4b1079c194
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2026-03-26 17:19:13 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -4,27 +4,22 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
         
-        for(int i = 0; i < T; i++) {
-            String S = br.readLine();
-            
-            Stack<Character> stack = new Stack<Character>();
-            
-            for(int j=0; j<S.length(); j++) {
-                if(S.charAt(j) == '(') {
-                    stack.push(S.charAt(j));
-                } else {
-                    if(stack.empty()) {
-                        stack.push(S.charAt(j));
-                        break;
-                    } else {
-                        stack.pop();
-                    }
+        for(int i=0; i<n; i++) {
+            Deque<Character> stack = new ArrayDeque<>();
+            String ps = br.readLine();
+            for(char c : ps.toCharArray()) {
+                if(c == '(') stack.push(c);
+                else {
+                    if(stack.size() != 0 && stack.peek() == '(') stack.poll();
+                    else  stack.push(c);
                 }
             }
-            if(stack.empty()) System.out.println("YES");
-            else System.out.println("NO");
+            sb.append(stack.size() == 0 ? "YES" : "NO").append('\n');            
         }
+        System.out.println(sb);
     }
 }
