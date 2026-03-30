9212abf6e03f9abf825167eec630c0429cdef544
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2026-03-30 15:09:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 90.48 ms, Memory: 97.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,22 +2,23 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
         HashMap<String, Integer> map = new HashMap<>();
+        String answer = "";
+        
         for(String s : participant) {
-            map.put(s, map.getOrDefault(s, 0) + 1);
+            map.put(s, map.getOrDefault(s, 0)+1);
         }
         
         for(String s : completion) {
-            map.put(s, map.get(s) - 1);
+            map.put(s, map.get(s)-1);
         }
         
         for(String s : map.keySet()) {
             if(map.get(s) > 0) {
                 answer = s;
-                break;
             }
         }
+        
         return answer;
     }
 }
