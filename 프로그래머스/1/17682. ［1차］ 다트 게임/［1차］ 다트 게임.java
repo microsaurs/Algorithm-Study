0db4fe41c0094307e6f8f8fd699b4f9e72cf20e1
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2026-03-31 14:52:50 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.08 ms, Memory: 86.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int solution(String dartResult) {
+        int[] result = new int[3];
+        int idx = -1;
+        
+        for(int i=0; i<dartResult.length(); i++) {
+            char c = dartResult.charAt(i);
+            
+            if(Character.isDigit(c)) {
+                idx++;
+                
+                if(c == '1' && i+1 <dartResult.length() && dartResult.charAt(i+1) == '0') {
+                    result[idx] = 10;
+                    i++;
+                } else {
+                    result[idx] = c-'0';
+                }
+            } else if(c == 'D') {
+                result[idx] = result[idx] * result[idx];
+            } else if(c == 'T') {
+                result[idx] = result[idx] * result[idx] * result[idx];
+            } else if (c == '*') {
+                result[idx] *= 2;
+                if(idx > 0) {
+                    result[idx - 1] *= 2;
+                }
+            } else if (c == '#') {
+                result[idx] *= -1;
+            }
+        }
+        return result[0] + result[1] + result[2];
+    }
+}
