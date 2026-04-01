26ec641c82489084ba18d99482cf349c6fce297f
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2026-04-01 14:00:27 +0900

    [level 2] Title: [1차] 캐시, Time: 37.91 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -2,9 +2,8 @@ import java.util.*;
 
 class Solution {
     public int solution(int cacheSize, String[] cities) {
-        int time = 0;
-        
         LinkedList<String> cache = new LinkedList<>();
+        int time = 0;
         
         for(String city : cities) {
             city = city.toLowerCase();
@@ -14,18 +13,16 @@ class Solution {
                 continue;
             }
             
-            // cache hit
-            if(cache.contains(city)) {
-                cache.remove(city);
-                cache.add(city);
-                time += 1;
-            }
-            else {
+            if(!cache.contains(city)) {
                 if(cache.size() == cacheSize) {
                     cache.removeFirst();
                 }
-                cache.add(city);
                 time += 5;
+                cache.add(city);
+            } else {
+                time += 1;
+                cache.remove(city);
+                cache.add(city);
             }
         }
         return time;
