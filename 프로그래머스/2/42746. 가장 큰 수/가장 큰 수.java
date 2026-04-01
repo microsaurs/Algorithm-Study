d893887c4d5017ad16596bf3222e4f08f3740793
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2026-04-01 16:29:42 +0900

    [level 2] Title: 가장 큰 수, Time: 278.65 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,23 +2,23 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String[] strNumbers = new String[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            strNumbers[i] = String.valueOf(numbers[i]);
+        String[] arr = new String[numbers.length];
+        
+        for(int i=0; i<numbers.length; i++) {
+            arr[i] = String.valueOf(numbers[i]);
         }
         
-        Arrays.sort(strNumbers, (a, b) -> (b + a).compareTo(a + b));
+        Arrays.sort(arr, (a, b) -> (b+a).compareTo(a+b));
         
-        if (strNumbers[0].equals("0")) {
+        if(arr[0].equals("0")) {
             return "0";
         }
         
-        StringBuilder answer = new StringBuilder();
-        for (String str : strNumbers) {
-            answer.append(str);
+        StringBuilder sb = new StringBuilder();
+        for(String s : arr) {
+            sb.append(s);
         }
         
-        return answer.toString();
-        
+        return sb.toString();
     }
 }
