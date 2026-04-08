6dd3664b7fc9aa8ff1622ffbcf470a7cda4914e0
JEONG SAE RYOUNG <105776815+microsaurs@users.noreply.github.com>
2026-04-08 20:29:10 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -1,20 +1,18 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        long[] fib = new long[91];
-        fib[0] = 0;
-        fib[1] = 1;
-        for (int i = 2; i <= 90; i++) {
-            fib[i] = fib[i-1] + fib[i-2];
+        long[] dp = new long[91];
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
         }
 
-        System.out.println(fib[n]);
-
+        System.out.println(dp[n]);
     }
 }
